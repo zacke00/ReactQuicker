f9fcf36c67a801d803313166c1b5dfb6f6a23ff0
Zakaria Berglund <zacke.berglund@outlook.com>
2024-09-04 12:59:12 +0200

    Update hamburger.tsx

--- reactquicker-web-app/src/components/hamburger/hamburger.tsx
+++ reactquicker-web-app/src/components/hamburger/hamburger.tsx
@@ -16,17 +16,17 @@ export function Hamburger(props: HamburgerProps) {
     return (
         <button onClick={handleClick} type='button' className={`w-8 h-8 flex justify-around flex-col flex-wrap z-10 cursor-pointer`}>
         <div
-          className={`bg-black block w-8 h-[0.35rem] rounded transition-all duration-400 ease-in-out origin-[1px] ${
+          className={`bg-black block w-8 h-[0.35rem] rounded transition-all duration-300 ease-in-out origin-[1px] ${
             isOpen ? 'rotate-45' : 'rotate-0'
           }`}
         />
         <div
-          className={`bg-black block w-8 h-[0.35rem] rounded transition-all duration-400 ease-in-out origin-[1px] ${
+          className={`bg-black block w-8 h-[0.35rem] rounded transition-all duration-300 ease-in-out origin-[1px] ${
             isOpen ? 'translate-x-full bg-transparent' : 'translate-x-0'
           }`}
         />
         <div
-          className={`bg-black block w-8 h-[0.35rem] rounded transition-all duration-400 ease-in-out origin-[1px] ${
+          className={`bg-black block w-8 h-[0.35rem] rounded transition-all duration-300 ease-in-out origin-[1px] ${
             isOpen ? 'rotate-[-45deg]' : 'rotate-0'
           }`}
         />
